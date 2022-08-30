6762207b4732dfa9bc42abac83194aedfecc55ae
Kobajaski <kobajaski@gmail.com>
2022-08-30 14:58:46 +0200

    fix: font themeé

--- install-theme.sh
+++ install-theme.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 TMP=$(which kpsewhich)
-FONTS_DIR=$HOME/.fonts
 if [ $? -ne 0 ]
 then
     echo 'ERROR ON THEME INSTALLATION : '$TMP
@@ -20,21 +19,6 @@ then
     echo 'ERROR ON THEME INSTALLATION'
     exit 1
 fi
-
-if [ ! -d "$FONTS_DIR" ]
-then
-    mkdir $FONTS_DIR
-fi
-
-unzip Gotham-font.zip -d $FONTS_DIR/
-
-if [ $? -ne 0 ]
-then
-    echo 'ERROR ON THEME INSTALLATION'
-    exit 1
-fi
-
-fc-cache -fv
 
 echo 'INSTALLATION SUCCEED'
 exit 0
